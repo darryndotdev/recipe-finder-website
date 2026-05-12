2c124ae3cd00ab6ce9f615c1e351feedea0f849d
Darryn <hi@darryn.dev>
2026-05-12 19:41:48 +0100

    chore: creating footer links and focus/hover styles

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styles from '@/components/layout/Footer/Footer.module.scss';
 import Container from '@/components/ui/Container/Container';
 import Instagram from '@/assets/icons/Instagram';
@@ -12,13 +13,19 @@ function Footer() {
                     <p>Made with ❤️ and 🥑</p>
                     <ul className={styles.list}>
                         <li>
-                            <Instagram />
+                            <Link to='#' className={styles.link}>
+                                <Instagram />
+                            </Link>
                         </li>
                         <li>
-                            <Bluesky />
+                            <Link to='#' className={styles.link}>
+                                <Bluesky />
+                            </Link>
                         </li>
                         <li>
-                            <Tiktok />
+                            <Link to='#' className={styles.link}>
+                                <Tiktok />
+                            </Link>
                         </li>
                     </ul>
                 </div>
