f7482e52ec8dbc8aa03962041fdb21618dd6d809
Darryn <hi@darryn.dev>
2026-05-17 19:17:18 +0100

    feat: creating mobile menu toggle

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 import styles from '@/components/layout/Navbar/Navbar.module.scss';
 import Container from '@/components/ui/Container/Container';
 import Button from '@/components/ui/Button/Button';
+import Menu from '@/assets/icons/Menu';
+import Cross from '@/assets/icons/Cross';
 import logo from '@/assets/icons/logo.svg';
-import menu from '@/assets/icons/icon-hamburger-menu.svg';
 
 const navLinks = [
     {
@@ -25,6 +27,8 @@ const navLinks = [
 ];
 
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <Container>
             <nav>
@@ -57,10 +61,38 @@ function Navbar() {
                     >
                         Browse recipes
                     </Button>
-                    <button className={styles.btn}>
-                        <img src={menu} alt='' />
+                    <button
+                        className={styles.btn}
+                        onClick={() => setIsOpen((prev) => !prev)}
+                    >
+                        {isOpen ? <Cross /> : <Menu />}
                     </button>
                 </div>
+                {isOpen && (
+                    <div className={styles.mobileNav}>
+                        <ul>
+                            {navLinks.map((link) => (
+                                <li key={link.id} className={styles.item}>
+                                    <Link
+                                        to={link.path}
+                                        className={styles.link}
+                                    >
+                                        {link.title}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                        <Button
+                            to='recipes'
+                            paddingY='150'
+                            paddingX='200'
+                            radius='lg'
+                            className={styles.browse}
+                        >
+                            Browse recipes
+                        </Button>
+                    </div>
+                )}
             </nav>
         </Container>
     );
